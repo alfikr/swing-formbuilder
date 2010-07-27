3c7a5b2f14273fc4a2d1659735dfcdc03a5926c9
eav1986 <eav1986@42cb3df5-4eff-ff36-babb-ce0de0ee3147>
2010-07-27 19:37:39 +0000

    some minor changes

--- trunk/src/formbuilder-parent/formbuilder-main/src/test/java/org/formbuilder/main/Person.java
+++ trunk/src/formbuilder-parent/formbuilder-main/src/test/java/org/formbuilder/main/Person.java
@@ -1,22 +1,21 @@
 /**
- * 
+ *
  */
 package org.formbuilder.main;
 
+import org.formbuilder.main.annotations.UIField;
+
 import java.util.Date;
 import java.util.Set;
-
-import org.formbuilder.main.annotations.Field;
 
 /**
  * @author aeremenok 2010
  */
 public class Person
 {
-    private String       name;
-    private int          age;
-    private Date         birthDate;
-
+    private String name;
+    private int age;
+    private Date birthDate;
     private Set<Account> accounts;
 
     public Set<Account> getAccounts()
@@ -29,13 +28,13 @@ public class Person
         return this.age;
     }
 
-    @Field( title = "Date of birth" )
+    @UIField( title = "Date of birth" )
     public Date getBirthDate()
     {
         return this.birthDate;
     }
 
-    @Field( title = "Person's first name" )
+    @UIField( title = "Person's first name" )
     public String getName()
     {
         return this.name;
