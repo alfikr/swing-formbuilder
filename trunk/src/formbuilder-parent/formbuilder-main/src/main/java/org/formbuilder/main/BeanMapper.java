e2b323cf7be6aae128fcf31fdec9e8a188c9a0f9
eav1986 <eav1986@42cb3df5-4eff-ff36-babb-ce0de0ee3147>
2010-07-28 10:59:47 +0000

    default mapping test passed

--- trunk/src/formbuilder-parent/formbuilder-main/src/main/java/org/formbuilder/main/BeanMapper.java
+++ trunk/src/formbuilder-parent/formbuilder-main/src/main/java/org/formbuilder/main/BeanMapper.java
@@ -3,14 +3,12 @@
  */
 package org.formbuilder.main;
 
-import javax.swing.JComponent;
-
 /**
  * @author aeremenok 2010
  * @param <B>
  */
 public interface BeanMapper<B>
 {
-    JComponent map( Class<B> beanClass,
+    Mapping map( Class<B> beanClass,
                     final TypeMappers typeMappers );
 }
--- trunk/src/formbuilder-parent/formbuilder-main/src/main/java/org/formbuilder/main/impl/GridBagMapper.java
+++ trunk/src/formbuilder-parent/formbuilder-main/src/main/java/org/formbuilder/main/impl/GridBagMapper.java
@@ -6,16 +6,12 @@ package org.formbuilder.main.impl;
 import org.apache.log4j.Logger;
 import org.formbuilder.main.AbstractBeanMapper;
 import org.formbuilder.main.MapperNotFoundException;
-import org.formbuilder.main.TypeMapper;
+import org.formbuilder.main.Mapping;
 import org.formbuilder.main.TypeMappers;
-import org.formbuilder.main.annotations.UIField;
 import org.formbuilder.main.util.GridBagPanel;
 
 import javax.swing.*;
 import java.beans.PropertyDescriptor;
-import java.lang.reflect.Method;
-
-import static org.formbuilder.main.util.TextUtil.capitalize;
 
 /**
  * @author aeremenok 2010
@@ -27,12 +23,13 @@ public class GridBagMapper<B>
     private static final Logger log = Logger.getLogger( GridBagMapper.class );
 
     @Override
-    public JComponent map( final Class<B> beanClass,
-                           final TypeMappers typeMappers )
+    public Mapping map( final Class<B> beanClass,
+                        final TypeMappers typeMappers )
     {
         assert SwingUtilities.isEventDispatchThread();
 
         final GridBagPanel gridBagPanel = new GridBagPanel();
+        Mapping mapping = new Mapping( gridBagPanel );
 
         final PropertyDescriptor[] propertyDescriptors = getBeanInfo( beanClass ).getPropertyDescriptors();
         for ( int i = 0; i < propertyDescriptors.length; i++ )
@@ -40,11 +37,11 @@ public class GridBagMapper<B>
             final PropertyDescriptor descriptor = propertyDescriptors[i];
             if ( isSupported( descriptor ) )
             {
-                gridBagPanel.add( createLabel( descriptor ), i, 0 );
+                gridBagPanel.add( createLabel( mapping, descriptor ), i, 0 );
 
                 try
                 {
-                    gridBagPanel.add( createEditor( typeMappers, descriptor ), i, 1 );
+                    gridBagPanel.add( createEditor( typeMappers, descriptor, mapping ), i, 1 );
                 }
                 catch ( MapperNotFoundException e )
                 {
@@ -53,62 +50,6 @@ public class GridBagMapper<B>
             }
         }
 
-        return gridBagPanel;
-    }
-
-    private JLabel createLabel( final PropertyDescriptor descriptor )
-    {
-        final JLabel label = new JLabel( getTitle( descriptor ) );
-        label.setName( descriptor.getName() );
-        return label;
-    }
-
-    private String getTitle( final PropertyDescriptor descriptor )
-    {
-        final Method readMethod = descriptor.getReadMethod();
-
-        UIField uiField = readMethod.getAnnotation( UIField.class );
-        if ( uiField != null )
-        {
-            return uiField.title();
-        }
-
-        String className = readMethod.getDeclaringClass().getSimpleName();
-        String propertyName = descriptor.getName();
-        String uiManagerTitle = UIManager.getString( className + "." + propertyName );
-        if ( uiManagerTitle != null )
-        {
-            return uiManagerTitle;
-        }
-
-        String displayName = descriptor.getDisplayName();
-        if ( displayName != null )
-        {
-            return capitalize( displayName );
-        }
-
-        return capitalize( descriptor.getName() );
-    }
-
-    private JComponent createEditor( final TypeMappers typeMappers,
-                                     final PropertyDescriptor descriptor )
-            throws
-            MapperNotFoundException
-    {
-        final TypeMapper mapper = typeMappers.getMapper( descriptor.getReadMethod() );
-        final JComponent component = mapper.createComponent();
-        component.setName( descriptor.getName() );
-        return component;
-    }
-
-    protected void handleNotFoundMapper( final MapperNotFoundException e )
-    {
-        // todo implement different strategies
-        log.warn( "Cannot find mapper for method " + e.getReadMethod() );
-    }
-
-    private boolean isSupported( final PropertyDescriptor descriptor )
-    {
-        return descriptor.getReadMethod() != null && !"class".equals( descriptor.getName() );
+        return mapping;
     }
 }
