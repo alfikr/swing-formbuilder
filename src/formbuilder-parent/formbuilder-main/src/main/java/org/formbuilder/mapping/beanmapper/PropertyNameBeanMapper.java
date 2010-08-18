e27ed4a3ea847f7a1f7468bb753e88e9b1389104
eav1986 <eav1986@42cb3df5-4eff-ff36-babb-ce0de0ee3147>
2010-08-18 12:55:28 +0000

    sample and property mapping contexts extracted, packages moved

---
+++ src/formbuilder-parent/formbuilder-main/src/main/java/org/formbuilder/mapping/beanmapper/PropertyNameBeanMapper.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2010 Andrey Yeremenok (eav1986__at__gmail__com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+package org.formbuilder.mapping.beanmapper;
+
+import org.formbuilder.BeanMapper;
+import org.formbuilder.mapping.BeanMapping;
+import org.formbuilder.mapping.BeanMappingContext;
+
+import javax.annotation.Nonnull;
+import javax.annotation.concurrent.NotThreadSafe;
+import javax.swing.*;
+import java.awt.*;
+
+/** @author aeremenok Date: Aug 3, 2010 Time: 1:09:26 PM */
+@NotThreadSafe
+public abstract class PropertyNameBeanMapper<B>
+        implements BeanMapper<B>
+{
+// ------------------------ INTERFACE METHODS ------------------------
+
+// --------------------- Interface BeanMapper ---------------------
+
+    @Override
+    public BeanMapping map( @Nonnull final BeanMappingContext<B> context )
+    {
+        final JPanel wrapper = new JPanel( new BorderLayout() );
+
+        final BeanMapping beanMapping = new BeanMapping( wrapper );
+        final PropertyContext<B> propertyContext = new PropertyContext<B>( context, beanMapping );
+
+        wrapper.add( mapBean( propertyContext ) );
+
+        return beanMapping;
+    }
+
+// -------------------------- OTHER METHODS --------------------------
+
+    protected abstract JComponent mapBean( final PropertyContext<B> context );
+}
