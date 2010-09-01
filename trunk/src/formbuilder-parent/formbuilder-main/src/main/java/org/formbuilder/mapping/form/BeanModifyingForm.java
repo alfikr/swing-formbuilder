dd8a623c74b893a9c43a394176ba4f243c4d3b30
eav1986 <eav1986@42cb3df5-4eff-ff36-babb-ce0de0ee3147>
2010-09-01 19:29:26 +0000

    #4 form factory tested

--- trunk/src/formbuilder-parent/formbuilder-main/src/main/java/org/formbuilder/mapping/form/BeanModifyingForm.java
+++ trunk/src/formbuilder-parent/formbuilder-main/src/main/java/org/formbuilder/mapping/form/BeanModifyingForm.java
@@ -13,12 +13,20 @@ package org.formbuilder.mapping.form;
 
 import org.formbuilder.Form;
 import org.formbuilder.mapping.BeanMapping;
+import org.formbuilder.util.Reflection;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.swing.*;
 
-/** @author aeremenok Date: Aug 31, 2010 Time: 12:49:55 PM */
+import static com.google.common.base.Preconditions.checkState;
+import static javax.swing.SwingUtilities.isEventDispatchThread;
+
+/**
+ * A {@link Form} that changes the given bean instance, or allocates a new one if a given instance is null
+ *
+ * @author aeremenok Date: Aug 31, 2010 Time: 12:49:55 PM
+ */
 public class BeanModifyingForm<B>
         implements Form<B>
 {
@@ -27,14 +35,17 @@ public class BeanModifyingForm<B>
     private B bean;
     private final JComponent panel;
     private final BeanMapping beanMapping;
+    private final Class<B> beanClass;
 
-// --------------------------- CONSTRUCTORS ---------------------------
+// --------------------------- CONSTRUCTORS -----------t----------------
 
     public BeanModifyingForm( @Nonnull final JComponent panel,
-                              @Nonnull final BeanMapping beanMapping )
+                              @Nonnull final BeanMapping beanMapping,
+                              @Nonnull final Class<B> beanClass )
     {
         this.panel = panel;
         this.beanMapping = beanMapping;
+        this.beanClass = beanClass;
     }
 
 // ------------------------ INTERFACE METHODS ------------------------
@@ -48,12 +59,19 @@ public class BeanModifyingForm<B>
 
     public B getValue()
     {
+        if ( bean == null )
+        {
+            bean = Reflection.newInstance( beanClass );
+        }
         beanMapping.setBeanValues( bean );
+        // todo set null if all values are empty?
         return bean;
     }
 
     public void setValue( @Nullable final B bean )
     {
+        checkState( isEventDispatchThread() );
         this.bean = bean;
+        beanMapping.setComponentValues( bean );
     }
 }
