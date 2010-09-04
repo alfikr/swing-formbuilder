e10568e98f4c5ef26eba44d47095b7bb1f8597c5
eav1986 <eav1986@42cb3df5-4eff-ff36-babb-ce0de0ee3147>
2010-09-04 16:50:07 +0000

    #6 interface mapping tests passed

--- src/formbuilder-parent/formbuilder-main/src/test/java/test/cases/InterfaceTest.java
+++ src/formbuilder-parent/formbuilder-main/src/test/java/test/cases/InterfaceTest.java
@@ -3,10 +3,19 @@ package test.cases;
 import domain.Address;
 import domain.AddressImpl;
 import org.fest.swing.fixture.ContainerFixture;
+import org.fest.swing.fixture.JPanelFixture;
 import org.formbuilder.Form;
 import org.formbuilder.FormBuilder;
+import org.formbuilder.mapping.beanmapper.SampleBeanMapper;
+import org.formbuilder.mapping.beanmapper.SampleContext;
 import org.testng.annotations.Test;
 
+import javax.annotation.Nonnull;
+import javax.swing.*;
+import java.awt.*;
+
+import static java.awt.BorderLayout.NORTH;
+import static java.awt.BorderLayout.SOUTH;
 import static org.formbuilder.mapping.form.FormFactories.MODIFYING;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNull;
@@ -61,5 +70,33 @@ public class InterfaceTest
 
         final Address newValue = form.getValue();
         assert newValue != null;
+    }
+
+    @Test( dependsOnMethods = "mapNullModifying" )
+    public void useSampleMapping()
+    {
+        final Form<Address> form = env.buildFormInEDT( FormBuilder.map( Address.class ).with( new SampleBeanMapper<Address>()
+        {
+            @Override
+            protected JComponent mapBean( @Nonnull final Address sample,
+                                          @Nonnull final SampleContext<Address> ctx )
+            {
+                final JPanel panel = new JPanel( new BorderLayout() );
+                panel.add( ctx.editor( sample.getHouse() ), NORTH );
+                panel.add( ctx.editor( sample.getStreet() ), SOUTH );
+                return panel;
+            }
+        } ) );
+        env.addToWindow( form );
+
+        final Address oldValue = new AddressImpl();
+        oldValue.setHouse( 7 );
+        oldValue.setStreet( "Main st." );
+        
+        env.setValueInEDT( form, oldValue );
+
+        final JPanelFixture wrapperPanel = env.getWrapperPanelFixture();
+        wrapperPanel.textBox( "street" ).requireText( "Main st." );
+        wrapperPanel.spinner( "house" ).requireValue( 7 );
     }
 }
